3caee3201d2a9e94f993bb7856f0f2b2c4e674a0
Saad <saadbinmahbub@gmail.com>
2014-04-11 14:47:00 +0600

    Added attribute to apache cookbook, apache package_name,service_name, document_root changes as platform changes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,23 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 #chef-repo/cookbooks/recipies/default.rb
-package_name = "apache2"
-service_name = "apache2"
-document_root = "/var/www/"
 
-if node["platform"] == "centos"
-	package_name = "httpd"
-	service_name = "httpd"
-	document_root = "/var/www/html"
-end
 
 #1. Install apache2
-package package_name do
+package node["package_name"] do
 	action :install
 end
 #2.Start the service
 #3. Make sure the service starts on reboot
-service service_name do
+service node["service_name"] do
 	action [:start, :enable]
 end
 #4.write our home page
@@ -32,7 +24,7 @@ end
 # 	mode "0644"
 # end
 
-template "#{document_root}/index.html" do
+template "#{node["document_root"]}/index.html" do
 	source "index.html.erb"
 	mode "0644"
 end
